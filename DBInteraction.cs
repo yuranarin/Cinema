2c844d74b56b2bd26df7c25b99e46f83f505c44c
jiura94 <jiura94@gmail.com>
2020-04-28 11:47:19 +0300

    Initial commit

---
+++ DBInteraction.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Cinema
+{
+    public static class DBInteraction
+    {
+        public struct Ticket
+        {
+            public string ticketId;
+            public string filmName;
+            public string cinemaName;
+            public DateTime filmDate;
+            public string cinemaAddress;
+            public string seatCode;
+        }
+
+        private static string ConnectionString =
+            $"Data Source={Properties.Resources.DBServerName};" +
+            $"Initial Catalog={Properties.Resources.DBInitialCatalog};"+
+            $"User id={Properties.Resources.DBUser};"+
+            $"Password={Properties.Resources.DBPassword};"+
+            $"Connection Timeout={Properties.Resources.ConnectionTimeout}";
+        /// <summary>
+        /// Searches for certain user in a database.
+        /// </summary>
+        /// <param name="username">User's login</param>
+        /// <param name="password">User's password</param>
+        /// <returns>If user exists then role, else raises an exception</returns>
+        public static string LoginCheck(string username, string password)
+        {
+            string query = $"select Role from User where Login = '{username}' and Password = '{password}'";
+            string output = "";
+            using(SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, con);
+                con.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)//if user exists
+                    {
+                        reader.Read();
+                        output = reader[0].ToString();//reader can't have more than 1 row due to database's integrity constraints
+                    }
+                    else
+                    {
+                        throw new Exception(Properties.Resources.UserNotFoundQueryResponse);
+                    }
+                }
+                return output;
+            }
+        }
+    }
+}
---
+++ Login.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace Cinema
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void BlockInputs()
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                LoginTextBox.IsEnabled = false;
+                PasswordBox.IsEnabled = false;
+                PasswordBox.Password = "";
+                LoginButton.IsEnabled = false;
+            });
+            for(var i = Properties.Settings.Default.LoginBlockTimeout; i>0; i--)
+            {
+                this.Dispatcher.Invoke(() => { LoginTextBox.Text = $"До конца блокировки осталось: {i} секунд."; });
+                Thread.Sleep(1000);
+            }
+            this.Dispatcher.Invoke(() =>
+            {
+                LoginTextBox.IsEnabled = true;
+                LoginTextBox.Text = "";
+                LoginTextBox.IsEnabled = true;
+                LoginButton.IsEnabled = true;
+            });
+            Properties.Settings.Default.LoginTries = 0;
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {            
+            string username = LoginTextBox.Text;
+            string password = PasswordBox.Password;
+            //WIP: anti-injection input tests
+            string userRole = "";
+            try
+            {
+                userRole = DBInteraction.LoginCheck(username, password);
+            }
+            catch(Exception ex)
+            {
+                if (ex.Message == Properties.Resources.UserNotFoundQueryResponse)
+                {
+                    MessageBox.Show($"Пользователь не найден. Проверьте логин и пароль.", "Ошибка");
+                    Properties.Settings.Default.LoginTries += 1;
+                    if (Properties.Settings.Default.LoginTries >= Properties.Settings.Default.LoginBlockLimit)
+                    {
+                        Thread t = new Thread(new ThreadStart(BlockInputs));
+                        t.Start();
+                        return;
+                    }
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show($"Произошла ошибка при попытке входа в систему: {ex.Message}. Обратитесь к администратору и попробуйте позже.", "Ошибка");
+                    return;
+                }
+            }
+
+            userRole = userRole.ToLower();
+            switch (userRole)
+            {
+                case "client":
+                    ClientInterface clientInterface = new ClientInterface();
+                    NavigationService.Navigate(clientInterface);
+                    break;
+                case "manager":
+                    ManagerInterface managerInterface = new ManagerInterface();
+                    NavigationService.Navigate(managerInterface);
+                    break;
+                case "admin":
+                    AdminInterface adminInterface = new AdminInterface();
+                    NavigationService.Navigate(adminInterface);
+                    break;
+
+                default:
+                    MessageBox.Show("Произошла ошибка при попытке входа в систему: Ваша роль неизвестна системе.", "Ошибка");
+                    break;
+            }
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
